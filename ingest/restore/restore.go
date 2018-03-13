5ffe07caf96993a09d75fab29c1fdffaa8b6b96c
silentsai <cenqichao@gmail.com>
2018-03-13 11:18:55 +0800

    correct config section name using

--- ingest/restore/restore.go
+++ ingest/restore/restore.go
@@ -458,7 +458,7 @@ func (tr *TableRestore) loadRegions() {
 	// TODO : regionProgress & !regionProgress.Finished()
 
 	preAllocateRowsID := !tr.tableInfo.WithIntegerPrimaryKey()
-	founder := mydump.NewRegionFounder(tr.cfg.Mydump.MinRegionSize)
+	founder := mydump.NewRegionFounder(tr.cfg.Mydumper.MinRegionSize)
 	regions := founder.MakeTableRegions(tr.tableMeta, preAllocateRowsID)
 
 	table := tr.tableMeta.Name
