9dd8a652d9ff0ca2218c5ee115af30b0ab2e672a
kennytm <kennytm@gmail.com>
2018-08-31 10:37:16 +0800

    restore: extract the close with retry operation into a function

--- lightning/restore/restore.go
+++ lightning/restore/restore.go
@@ -227,26 +227,8 @@ func (rc *RestoreController) restoreTable(ctx context.Context, t *TableRestore, 
 	wg.Wait()
 	common.AppLogger.Infof("[%s] encode kv data and write takes %v", table, time.Since(timer))
 
-	// ensure the engine is closed (thus the memory for tikv-importer is freed)
-	// before recycling the table workers.
-	kvDeliver := t.deliversMgr.AcquireClient(t.tableMeta.DB, t.tableMeta.Name)
-	var err error
-	for i := 1; i <= closeEngineMaxRetry; i++ {
-		err = kvDeliver.CloseEngine()
-		if err == nil {
-			break
-		}
-		if strings.Contains(err.Error(), "EngineInUse") && i < closeEngineMaxRetry {
-			common.AppLogger.Infof("engine still not fully closed, retrying after %d.0s %v", i, err)
-			time.Sleep(time.Duration(i) * time.Second)
-		} else {
-			common.AppLogger.Errorf("[kv-deliver] flush stage with error (step = close) : %s", errors.ErrorStack(err))
-			break
-		}
-	}
-	t.deliversMgr.RecycleClient(kvDeliver)
-
 	// recycle table worker in advance to prevent so many idle region workers and promote CPU utilization.
+	err := t.closeWithRetry()
 	rc.tableWorkers.Recycle(w)
 	if ctxAborted {
 		return errCtxAborted
@@ -270,6 +252,31 @@ func (rc *RestoreController) restoreTable(ctx context.Context, t *TableRestore, 
 	t.tableRows = tableRows
 
 	err = rc.postProcessing(t)
+	return errors.Trace(err)
+}
+
+func (t *TableRestore) closeWithRetry() error {
+	// ensure the engine is closed (thus the memory for tikv-importer is freed)
+	// before recycling the table workers.
+	kvDeliver := t.deliversMgr.AcquireClient(t.tableMeta.DB, t.tableMeta.Name)
+	defer t.deliversMgr.RecycleClient(kvDeliver)
+
+	var err error
+
+	for i := 1; i <= closeEngineMaxRetry; i++ {
+		err := kvDeliver.CloseEngine()
+		if err == nil {
+			return nil
+		}
+		if !strings.Contains(err.Error(), "EngineInUse") || i == closeEngineMaxRetry {
+			// error cannot be recovered, return immediately
+			break
+		}
+		common.AppLogger.Infof("engine still not fully closed, retrying after %d.0s : %s", i, err)
+		time.Sleep(time.Duration(i) * time.Second)
+	}
+
+	common.AppLogger.Errorf("[kv-deliver] flush stage with error (step = close) : %s", errors.ErrorStack(err))
 	return errors.Trace(err)
 }
 
