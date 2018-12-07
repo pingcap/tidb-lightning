pingcap/tidb-lightning

commit 7df35e95a1ed7c2cf19136a545c7f78cd772d426

Parallelize ANALYZE calls (#91)
* config: exposed all session concurrency variables

* restore: run ANALYZE concurrently in the post-restore step

* config: increase default read-block-size to 65536

* tests: reduces chance of spurious failure in checkpoint_chunks test

* restore: addressed comments

lightning/config/config.go
@@ -21,15 +21,19 @@ const (
 )
 
 type DBStore struct {
-	Host                   string `toml:"host" json:"host"`
-	Port                   int    `toml:"port" json:"port"`
-	User                   string `toml:"user" json:"user"`
-	Psw                    string `toml:"password" json:"-"`
-	StatusPort             int    `toml:"status-port" json:"status-port"`
-	PdAddr                 string `toml:"pd-addr" json:"pd-addr"`
-	SQLMode                string `toml:"sql-mode" json:"sql-mode"`
-	LogLevel               string `toml:"log-level" json:"log-level"`
-	DistSQLScanConcurrency int    `toml:"distsql-scan-concurrency" json:"distsql-scan-concurrency"`
+	Host       string `toml:"host" json:"host"`
+	Port       int    `toml:"port" json:"port"`
+	User       string `toml:"user" json:"user"`
+	Psw        string `toml:"password" json:"-"`
+	StatusPort int    `toml:"status-port" json:"status-port"`
+	PdAddr     string `toml:"pd-addr" json:"pd-addr"`
+	SQLMode    string `toml:"sql-mode" json:"sql-mode"`
+	LogLevel   string `toml:"log-level" json:"log-level"`
+
+	DistSQLScanConcurrency     int `toml:"distsql-scan-concurrency" json:"distsql-scan-concurrency"`
+	BuildStatsConcurrency      int `toml:"build-stats-concurrency" json:"build-stats-concurrency"`
+	IndexSerialScanConcurrency int `toml:"index-serial-scan-concurrency" json:"index-serial-scan-concurrency"`
+	ChecksumTableConcurrency   int `toml:"checksum-table-concurrency" json:"checksum-table-concurrency"`
 }
 
 type Config struct {
@@ -104,8 +108,11 @@ func NewConfig() *Config {
 			CheckRequirements: true,
 		},
 		TiDB: DBStore{
-			SQLMode:                "STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION",
-			DistSQLScanConcurrency: 16,
+			SQLMode:                    "STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION",
+			BuildStatsConcurrency:      20,
+			DistSQLScanConcurrency:     100,
+			IndexSerialScanConcurrency: 20,
+			ChecksumTableConcurrency:   16,
 		},
 	}
 }

lightning/restore/restore.go
@@ -92,6 +92,7 @@ type RestoreController struct {
 	tableWorkers    *RestoreWorkerPool
 	regionWorkers   *RestoreWorkerPool
 	importer        *kv.Importer
+	tidbMgr         *TiDBManager
 	postProcessLock sync.Mutex // a simple way to ensure post-processing is not concurrent without using complicated goroutines
 	alterTableLock  sync.Mutex
 
@@ -113,12 +114,18 @@ func NewRestoreController(ctx context.Context, dbMetas map[string]*mydump.MDData
 		return nil, errors.Trace(err)
 	}
 
+	tidbMgr, err := NewTiDBManager(cfg.TiDB)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
 	rc := &RestoreController{
 		cfg:           cfg,
 		dbMetas:       dbMetas,
 		tableWorkers:  NewRestoreWorkerPool(ctx, cfg.App.TableConcurrency, "table"),
 		regionWorkers: NewRestoreWorkerPool(ctx, cfg.App.RegionConcurrency, "region"),
 		importer:      importer,
+		tidbMgr:       tidbMgr,
 
 		errorSummaries: errorSummaries{
 			summary: make(map[string]errorSummary),
@@ -153,6 +160,7 @@ func (rc *RestoreController) Wait() {
 
 func (rc *RestoreController) Close() {
 	rc.importer.Close()
+	rc.tidbMgr.Close()
 }
 
 func (rc *RestoreController) Run(ctx context.Context) error {
@@ -163,7 +171,6 @@ func (rc *RestoreController) Run(ctx context.Context) error {
 		rc.restoreSchema,
 		rc.restoreTables,
 		rc.fullCompact,
-		rc.analyze,
 		rc.switchToNormalMode,
 		rc.cleanCheckpoints,
 	}
@@ -529,10 +536,12 @@ func (t *TableRestore) postProcess(ctx context.Context, closedEngine *kv.ClosedE
 		}
 	}
 
+	setSessionConcurrencyVars(ctx, rc.tidbMgr.db, rc.cfg.TiDB)
+
 	// 3. alter table set auto_increment
 	if cp.Status < CheckpointStatusAlteredAutoInc {
 		rc.alterTableLock.Lock()
-		err := t.restoreTableMeta(ctx, rc.cfg)
+		err := t.restoreTableMeta(ctx, rc.tidbMgr.db)
 		rc.alterTableLock.Unlock()
 		rc.saveStatusCheckpoint(t.tableName, err, CheckpointStatusAlteredAutoInc)
 		if err != nil {
@@ -545,12 +554,32 @@ func (t *TableRestore) postProcess(ctx context.Context, closedEngine *kv.ClosedE
 	}
 
 	// 4. do table checksum
-	if cp.Status < CheckpointStatusCompleted {
-		err := t.compareChecksum(ctx, rc.cfg, cp)
-		rc.saveStatusCheckpoint(t.tableName, err, CheckpointStatusCompleted)
-		if err != nil {
-			common.AppLogger.Errorf("[%s] checksum failed: %v", t.tableName, err.Error())
-			return errors.Trace(err)
+	if cp.Status < CheckpointStatusChecksummed {
+		if !rc.cfg.PostRestore.Checksum {
+			common.AppLogger.Infof("[%s] Skip checksum.", t.tableName)
+			rc.saveStatusCheckpoint(t.tableName, nil, CheckpointStatusChecksumSkipped)
+		} else {
+			err := t.compareChecksum(ctx, rc.tidbMgr.db, cp)
+			rc.saveStatusCheckpoint(t.tableName, nil, CheckpointStatusChecksummed)
+			if err != nil {
+				common.AppLogger.Errorf("[%s] checksum failed: %v", t.tableName, err.Error())
+				return errors.Trace(err)
+			}
+		}
+	}
+
+	// 5. do table analyze
+	if cp.Status < CheckpointStatusAnalyzed {
+		if !rc.cfg.PostRestore.Analyze {
+			common.AppLogger.Infof("[%s] Skip analyze.", t.tableName)
+			rc.saveStatusCheckpoint(t.tableName, nil, CheckpointStatusAnalyzeSkipped)
+		} else {
+			err := t.analyzeTable(ctx, rc.tidbMgr.db)
+			rc.saveStatusCheckpoint(t.tableName, err, CheckpointStatusAnalyzed)
+			if err != nil {
+				common.AppLogger.Errorf("[%s] analyze failed: %v", t.tableName, err.Error())
+				return errors.Trace(err)
+			}
 		}
 	}
 
@@ -569,18 +598,6 @@ func (rc *RestoreController) fullCompact(ctx context.Context) error {
 
 func (rc *RestoreController) doCompact(ctx context.Context, level int32) error {
 	return errors.Trace(rc.importer.Compact(ctx, level))
-}
-
-// analyze will analyze table for all tables.
-func (rc *RestoreController) analyze(ctx context.Context) error {
-	if !rc.cfg.PostRestore.Analyze {
-		common.AppLogger.Info("Skip analyze table.")
-		return nil
-	}
-
-	tables := rc.getTables()
-	err := analyzeTable(ctx, rc.cfg.TiDB, tables)
-	return errors.Trace(err)
 }
 
 func (rc *RestoreController) switchToImportMode(ctx context.Context) error {
@@ -739,49 +756,6 @@ func (rc *RestoreController) getTables() []string {
 	}
 
 	return tables
-}
-
-func analyzeTable(ctx context.Context, dsn config.DBStore, tables []string) error {
-	totalTimer := time.Now()
-	db, err := common.ConnectDB(dsn.Host, dsn.Port, dsn.User, dsn.Psw)
-	if err != nil {
-		common.AppLogger.Errorf("connect db failed %v, the next operation is: ANALYZE TABLE. You should do it one by one manually", err)
-		return errors.Trace(err)
-	}
-	defer db.Close()
-
-	// speed up executing analyze table temporarily
-	setSessionVarInt(ctx, db, "tidb_build_stats_concurrency", 16)
-	setSessionVarInt(ctx, db, "tidb_distsql_scan_concurrency", dsn.DistSQLScanConcurrency)
-
-	// TODO: do it concurrently.
-	var analyzeErr error
-	for _, table := range tables {
-		timer := time.Now()
-		common.AppLogger.Infof("[%s] analyze", table)
-		query := fmt.Sprintf("ANALYZE TABLE %s", table)
-		err := common.ExecWithRetry(ctx, db, query, query)
-		if err != nil {
-			if analyzeErr == nil {
-				analyzeErr = err
-			}
-			common.AppLogger.Errorf("%s error %s", query, errors.ErrorStack(err))
-			continue
-		}
-		common.AppLogger.Infof("[%s] analyze takes %v", table, time.Since(timer))
-	}
-
-	common.AppLogger.Infof("doing all tables analyze takes %v", time.Since(totalTimer))
-	return errors.Trace(analyzeErr)
-}
-
-////////////////////////////////////////////////////////////////
-
-func setSessionVarInt(ctx context.Context, db *sql.DB, name string, value int) {
-	stmt := fmt.Sprintf("set session %s = ?", name)
-	if err := common.ExecWithRetry(ctx, db, stmt, stmt, value); err != nil {
-		common.AppLogger.Warnf("failed to set variable @%s to %d: %s", name, value, err.Error())
-	}
 }
 
 ////////////////////////////////////////////////////////////////
@@ -947,16 +921,10 @@ func (t *TableRestore) populateChunks(minChunkSize int64, cp *TableCheckpoint, t
 	return nil
 }
 
-func (tr *TableRestore) restoreTableMeta(ctx context.Context, cfg *config.Config) error {
+func (tr *TableRestore) restoreTableMeta(ctx context.Context, db *sql.DB) error {
 	timer := time.Now()
-	dsn := cfg.TiDB
-	db, err := common.ConnectDB(dsn.Host, dsn.Port, dsn.User, dsn.Psw)
-	if err != nil {
-		return errors.Trace(err)
-	}
-	defer db.Close()
 
-	err = AlterAutoIncrement(ctx, db, tr.tableMeta.DB, tr.tableMeta.Name, tr.alloc.Base()+1)
+	err := AlterAutoIncrement(ctx, db, tr.tableMeta.DB, tr.tableMeta.Name, tr.alloc.Base()+1)
 	if err != nil {
 		return errors.Trace(err)
 	}
@@ -986,19 +954,14 @@ func (tr *TableRestore) importKV(ctx context.Context, closedEngine *kv.ClosedEng
 }
 
 // do checksum for each table.
-func (tr *TableRestore) compareChecksum(ctx context.Context, cfg *config.Config, cp *TableCheckpoint) error {
-	if !cfg.PostRestore.Checksum {
-		common.AppLogger.Infof("[%s] Skip checksum.", tr.tableName)
-		return nil
-	}
-
+func (tr *TableRestore) compareChecksum(ctx context.Context, db *sql.DB, cp *TableCheckpoint) error {
 	var localChecksum verify.KVChecksum
 	for _, chunk := range cp.Chunks {
 		localChecksum.Add(&chunk.Checksum)
 	}
 
 	start := time.Now()
-	remoteChecksum, err := DoChecksum(ctx, cfg.TiDB, tr.tableName)
+	remoteChecksum, err := DoChecksum(ctx, db, tr.tableName)
 	dur := time.Since(start)
 	metric.ChecksumSecondsHistogram.Observe(dur.Seconds())
 	if err != nil {
@@ -1019,6 +982,18 @@ func (tr *TableRestore) compareChecksum(ctx context.Context, cfg *config.Config,
 	return nil
 }
 
+func (tr *TableRestore) analyzeTable(ctx context.Context, db *sql.DB) error {
+	timer := time.Now()
+	common.AppLogger.Infof("[%s] analyze", tr.tableName)
+	query := fmt.Sprintf("ANALYZE TABLE %s", tr.tableName)
+	err := common.ExecWithRetry(ctx, db, query, query)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	common.AppLogger.Infof("[%s] analyze takes %v", tr.tableName, time.Since(timer))
+	return nil
+}
+
 // RemoteChecksum represents a checksum result got from tidb.
 type RemoteChecksum struct {
 	Schema     string
@@ -1032,15 +1007,22 @@ func (c *RemoteChecksum) String() string {
 	return fmt.Sprintf("[%s] remote_checksum=%d, total_kvs=%d, total_bytes=%d", common.UniqueTable(c.Schema, c.Table), c.Checksum, c.TotalKVs, c.TotalBytes)
 }
 
+func setSessionConcurrencyVars(ctx context.Context, db *sql.DB, dsn config.DBStore) {
+	err := common.ExecWithRetry(ctx, db, "(set session concurrency variables)", `SET
+		SESSION tidb_build_stats_concurrency = ?,
+		SESSION tidb_distsql_scan_concurrency = ?,
+		SESSION tidb_index_serial_scan_concurrency = ?,
+		SESSION tidb_checksum_table_concurrency = ?;
+	`, dsn.BuildStatsConcurrency, dsn.DistSQLScanConcurrency, dsn.IndexSerialScanConcurrency, dsn.ChecksumTableConcurrency)
+	if err != nil {
+		common.AppLogger.Warnf("failed to set session concurrency variables: %s", err.Error())
+	}
+}
+
 // DoChecksum do checksum for tables.
 // table should be in <db>.<table>, format.  e.g. foo.bar
-func DoChecksum(ctx context.Context, dsn config.DBStore, table string) (*RemoteChecksum, error) {
+func DoChecksum(ctx context.Context, db *sql.DB, table string) (*RemoteChecksum, error) {
 	timer := time.Now()
-	db, err := common.ConnectDB(dsn.Host, dsn.Port, dsn.User, dsn.Psw)
-	if err != nil {
-		return nil, errors.Trace(err)
-	}
-	defer db.Close()
 
 	ori, err := increaseGCLifeTime(ctx, db)
 	if err != nil {
@@ -1053,11 +1035,6 @@ func DoChecksum(ctx context.Context, dsn config.DBStore, table string) (*RemoteC
 			common.AppLogger.Errorf("[%s] update tikv_gc_life_time error %v", table, errors.ErrorStack(err))
 		}
 	}()
-
-	// speed up executing checksum table temporarily
-	// FIXME: now we do table checksum separately, will it be too frequent to update these variables?
-	setSessionVarInt(ctx, db, "tidb_checksum_table_concurrency", 16)
-	setSessionVarInt(ctx, db, "tidb_distsql_scan_concurrency", dsn.DistSQLScanConcurrency)
 
 	// ADMIN CHECKSUM TABLE <table>,<table>  example.
 	// 	mysql> admin checksum table test.t;
@@ -1124,7 +1101,7 @@ func splitIntoDeliveryStreams(totalKVs []kvenc.KvPair, splitSize int) [][]kvenc.
 
 	for j, pair := range totalKVs {
 		size := len(pair.Key) + len(pair.Val)
-		if i < j && cumSize + size > splitSize {
+		if i < j && cumSize+size > splitSize {
 			res = append(res, totalKVs[i:j])
 			i = j
 			cumSize = 0
@@ -1175,7 +1152,7 @@ func (cr *chunkRestore) restore(
 		chunkRowID      int64
 	}
 	block.cond = sync.NewCond(new(sync.Mutex))
-	deliverCompleteCh := make(chan error)
+	deliverCompleteCh := make(chan error, 1)
 
 	go func() {
 		for {
@@ -1319,7 +1296,7 @@ func (cr *chunkRestore) restore(
 		}
 
 		block.cond.L.Lock()
-		for len(block.totalKVs) > len(kvs) * maxKVQueueSize {
+		for len(block.totalKVs) > len(kvs)*maxKVQueueSize {
 			// ^ hack to create a back-pressure preventing sending too many KV pairs at once
 			// this happens when delivery is slower than encoding.
 			// note that the KV pairs will retain the memory buffer backing the KV encoder

tidb-lightning.toml
@@ -43,7 +43,7 @@ batch-size = 500 # GB
 
 [mydumper]
 # block size of file reading
-read-block-size = 4096 # Byte (default = 4 KB)
+read-block-size = 65536 # Byte (default = 64 KB)
 # split source data file into multiple region/chunk to execute restoring in parallel
 region-min-size = 268435456 # Byte (default = 256 MB)
 # mydumper local source data directory
@@ -69,11 +69,16 @@ status-port = 10080
 pd-addr = "127.0.0.1:2379"
 # lightning uses some code of tidb(used as library), and the flag controls it's log level.
 log-level = "error"
-# set tidb session variable to speed up checksum/analyze table.
-distsql-scan-concurrency = 16
+
+# set tidb session variables to speed up checksum/analyze table.
+# see https://pingcap.com/docs/sql/statistics/#control-analyze-concurrency for the meaning of each setting
+build-stats-concurrency = 20
+distsql-scan-concurrency = 100
+index-serial-scan-concurrency = 20
+checksum-table-concurrency = 16
 
 # post-restore provide some options which will be executed after all kv data has been imported into the tikv cluster.
-# the execution order are(if set true): checksum -> compact -> analyze
+# the execution order are(if set true): checksum -> analyze
 [post-restore]
 # if set true, checksum will do ADMIN CHECKSUM TABLE <table> for each table.
 checksum = true

tests/checkpoint/run.sh
@@ -75,5 +75,5 @@ echo "******** Verify checkpoint no-op ********"
 PIDFILE="$PIDFILE" run_lightning
 run_sql "$PARTIAL_IMPORT_QUERY"
 check_contains "s: $(( (1000 * $CHUNK_COUNT + 1001) * $CHUNK_COUNT * $TABLE_COUNT ))"
-run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cppk.table_v1 WHERE status = 180"
+run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cppk.table_v1 WHERE status = 210"
 check_contains "count(*): $TABLE_COUNT"

tests/checkpoint_chunks/run.sh
@@ -52,7 +52,7 @@ PIDFILE="$PIDFILE" run_lightning
 run_sql 'SELECT count(i), sum(i) FROM cpch_tsr.tbl;'
 check_contains "count(i): $(($ROW_COUNT*$CHUNK_COUNT))"
 check_contains "sum(i): $(( $ROW_COUNT*$CHUNK_COUNT*(($CHUNK_COUNT+2)*$ROW_COUNT + 1)/2 ))"
-run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cpch.table_v1 WHERE status = 180"
+run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cpch.table_v1 WHERE status = 210"
 check_contains "count(*): 1"
 run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cpch.chunk_v3 WHERE pos = end_offset"
 check_contains "count(*): $CHUNK_COUNT"

lightning/config/const.go
@@ -6,7 +6,7 @@ const (
 	_G = _M << 10
 
 	// mydumper
-	ReadBlockSize int64 = 32 * _K
+	ReadBlockSize int64 = 64 * _K
 	MinRegionSize int64 = 256 * _M
 
 	// kv import

commit 7ca4e9655f4e979dfd51429de835cff13caeb017

Serialize Compact; Run SwitchToImportMode periodically; Log progress periodically (#92)
* *: run Compact, SwitchToImportMode and log a progress periodically

* restore: ensure FullCompact and Level1Compact are also serial

Fixed an invalid usage of metrics.

* tests: fixed integration test failure caused by #91

* config, restore: fixed comments

* config, restore: start compaction asap (but still in seequential order)

go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/pingcap/tidb-enterprise-tools v1.0.1-0.20181116033341-5832f7307d74
 	github.com/pkg/errors v0.8.0
 	github.com/prometheus/client_golang v0.9.0
+	github.com/prometheus/client_model v0.0.0-20180712105110-5c3871d89910
 	github.com/satori/go.uuid v1.2.0
 	github.com/siddontang/go v0.0.0-20170517070808-cb568a3e5cc0 // indirect
 	github.com/sirupsen/logrus v1.2.0

lightning/config/config.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"runtime"
+	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/pingcap/tidb-enterprise-tools/pkg/filter"
@@ -49,6 +50,7 @@ type Config struct {
 	BWList       *filter.Rules   `toml:"black-white-list" json:"black-white-list"`
 	TikvImporter TikvImporter    `toml:"tikv-importer" json:"tikv-importer"`
 	PostRestore  PostRestore     `toml:"post-restore" json:"post-restore"`
+	Cron         Cron            `toml:"cron" json:"cron"`
 
 	// command line flags
 	ConfigFile   string `json:"config-file"`
@@ -101,6 +103,27 @@ type Checkpoint struct {
 	KeepAfterSuccess bool   `toml:"keep-after-success" json:"keep-after-success"`
 }
 
+type Cron struct {
+	SwitchMode  Duration `toml:"switch-mode" json:"switch-mode"`
+	LogProgress Duration `toml:"log-progress" json:"log-progress"`
+}
+
+// A duration which can be deserialized from a TOML string.
+// Implemented as https://github.com/BurntSushi/toml#using-the-encodingtextunmarshaler-interface
+type Duration struct {
+	time.Duration
+}
+
+func (d *Duration) UnmarshalText(text []byte) error {
+	var err error
+	d.Duration, err = time.ParseDuration(string(text))
+	return err
+}
+
+func (d *Duration) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`"%s"`, d.Duration)), nil
+}
+
 func NewConfig() *Config {
 	return &Config{
 		App: Lightning{
@@ -114,6 +137,10 @@ func NewConfig() *Config {
 			DistSQLScanConcurrency:     100,
 			IndexSerialScanConcurrency: 20,
 			ChecksumTableConcurrency:   16,
+		},
+		Cron: Cron{
+			SwitchMode:  Duration{Duration: 5 * time.Minute},
+			LogProgress: Duration{Duration: 5 * time.Minute},
 		},
 	}
 }

lightning/metric/metric.go
@@ -1,7 +1,10 @@
 package metric
 
 import (
+	"math"
+
 	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
 )
 
 var (
@@ -156,4 +159,22 @@ func RecordTableCount(status string, err error) {
 		result = TableResultSuccess
 	}
 	TableCounter.WithLabelValues(status, result).Inc()
+}
+
+// ReadCounter reports the current value of the counter.
+func ReadCounter(counter prometheus.Counter) float64 {
+	var metric dto.Metric
+	if err := counter.Write(&metric); err != nil {
+		return math.NaN()
+	}
+	return metric.Counter.GetValue()
+}
+
+// ReadCounter reports the sum of all observed values in the histogram.
+func ReadHistogramSum(histogram prometheus.Histogram) float64 {
+	var metric dto.Metric
+	if err := histogram.Write(&metric); err != nil {
+		return math.NaN()
+	}
+	return metric.Histogram.GetSampleSum()
 }

lightning/restore/restore.go
@@ -39,6 +39,11 @@ const (
 	defaultGCLifeTime = 100 * time.Hour
 )
 
+const (
+	compactStateIdle int32 = iota
+	compactStateDoing
+)
+
 var (
 	requiredTiDBVersion = *semver.New("2.0.4")
 	requiredPDVersion   = *semver.New("2.0.4")
@@ -95,6 +100,7 @@ type RestoreController struct {
 	tidbMgr         *TiDBManager
 	postProcessLock sync.Mutex // a simple way to ensure post-processing is not concurrent without using complicated goroutines
 	alterTableLock  sync.Mutex
+	compactState    int32
 
 	errorSummaries errorSummaries
 
@@ -177,7 +183,6 @@ func (rc *RestoreController) Run(ctx context.Context) error {
 	timer := time.Now()
 	opts := []func(context.Context) error{
 		rc.checkRequirements,
-		rc.switchToImportMode,
 		rc.restoreSchema,
 		rc.restoreTables,
 		rc.fullCompact,
@@ -196,7 +201,7 @@ outside:
 			err = nil
 			break outside
 		default:
-			common.AppLogger.Errorf("run cause error : %s", errors.ErrorStack(err))
+			common.AppLogger.Errorf("run cause error : %v", err)
 			fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 			break outside // ps : not continue
 		}
@@ -263,7 +268,7 @@ func (rc *RestoreController) estimateChunkCountIntoMetrics() {
 			}
 		}
 	}
-	metric.ChunkCounter.WithLabelValues("estimated").Add(float64(estimatedChunkCount))
+	metric.ChunkCounter.WithLabelValues(metric.ChunkStateEstimated).Add(float64(estimatedChunkCount))
 }
 
 func (rc *RestoreController) saveStatusCheckpoint(tableName string, err error, statusIfSucceed CheckpointStatus) {
@@ -322,6 +327,60 @@ func (rc *RestoreController) listenCheckpointUpdates(wg *sync.WaitGroup) {
 	}
 }
 
+func (rc *RestoreController) runPeriodicActions(ctx context.Context, stop <-chan struct{}) {
+	switchModeTicker := time.NewTicker(rc.cfg.Cron.SwitchMode.Duration)
+	logProgressTicker := time.NewTicker(rc.cfg.Cron.LogProgress.Duration)
+	defer func() {
+		switchModeTicker.Stop()
+		logProgressTicker.Stop()
+	}()
+
+	rc.switchToImportMode(ctx)
+
+	start := time.Now()
+
+	for {
+		select {
+		case <-ctx.Done():
+			common.AppLogger.Warnf("Stopping periodic actions due to %v", ctx.Err())
+			return
+		case <-stop:
+			common.AppLogger.Info("Everything imported, stopping periodic actions")
+			return
+
+		case <-switchModeTicker.C:
+			// periodically switch to import mode, as requested by TiKV 3.0
+			rc.switchToImportMode(ctx)
+
+		case <-logProgressTicker.C:
+			// log the current progress periodically, so OPS will know that we're still working
+			nanoseconds := float64(time.Since(start).Nanoseconds())
+			estimated := metric.ReadCounter(metric.ChunkCounter.WithLabelValues(metric.ChunkStateEstimated))
+			finished := metric.ReadCounter(metric.ChunkCounter.WithLabelValues(metric.ChunkStateFinished))
+			totalTables := metric.ReadCounter(metric.TableCounter.WithLabelValues(metric.TableStatePending, metric.TableResultSuccess))
+			completedTables := metric.ReadCounter(metric.TableCounter.WithLabelValues(metric.TableStateCompleted, metric.TableResultSuccess))
+			bytesRead := metric.ReadHistogramSum(metric.BlockReadBytesHistogram)
+
+			var remaining string
+			if finished >= estimated {
+				remaining = ", post-processing"
+			} else if finished > 0 {
+				remainNanoseconds := (estimated/finished - 1) * nanoseconds
+				remaining = fmt.Sprintf(", remaining %s", time.Duration(remainNanoseconds).Round(time.Second))
+			}
+
+			// Note: a speed of 28 MiB/s roughly corresponds to 100 GiB/hour.
+			common.AppLogger.Infof(
+				"progress: %.0f/%.0f chunks (%.1f%%), %.0f/%.0f tables (%.1f%%), speed %.2f MiB/s%s",
+				finished, estimated, finished/estimated*100,
+				completedTables, totalTables, completedTables/totalTables*100,
+				bytesRead/(1048576e-9*nanoseconds),
+				remaining,
+			)
+		}
+	}
+}
+
 func (rc *RestoreController) restoreTables(ctx context.Context) error {
 	timer := time.Now()
 	var wg sync.WaitGroup
@@ -330,6 +389,9 @@ func (rc *RestoreController) restoreTables(ctx context.Context) error {
 		restoreErrLock sync.Mutex
 		restoreErr     error
 	)
+
+	stopPeriodicActions := make(chan struct{}, 1)
+	go rc.runPeriodicActions(ctx, stopPeriodicActions)
 
 	for dbName, dbMeta := range rc.dbMetas {
 		dbInfo, ok := rc.dbInfos[dbName]
@@ -393,6 +455,7 @@ func (rc *RestoreController) restoreTables(ctx context.Context) error {
 	}
 
 	wg.Wait()
+	stopPeriodicActions <- struct{}{}
 	common.AppLogger.Infof("restore all tables data takes %v", time.Since(timer))
 
 	restoreErrLock.Lock()
@@ -538,11 +601,16 @@ func (t *TableRestore) postProcess(ctx context.Context, closedEngine *kv.ClosedE
 			return errors.Trace(err)
 		}
 
-		// 2. compact level 1
-		err = rc.doCompact(ctx, Level1Compact)
-		if err != nil {
-			// log it and continue
-			common.AppLogger.Warnf("[%s] do compact %d failed err %v", t.tableName, Level1Compact, errors.ErrorStack(err))
+		// 2. perform a level-1 compact if idling.
+		if atomic.CompareAndSwapInt32(&rc.compactState, compactStateIdle, compactStateDoing) {
+			go func() {
+				err := rc.doCompact(ctx, Level1Compact)
+				if err != nil {
+					// log it and continue
+					common.AppLogger.Warnf("compact %d failed %v", Level1Compact, err)
+				}
+				atomic.StoreInt32(&rc.compactState, compactStateIdle)
+			}()
 		}
 	}
 
@@ -570,7 +638,7 @@ func (t *TableRestore) postProcess(ctx context.Context, closedEngine *kv.ClosedE
 			rc.saveStatusCheckpoint(t.tableName, nil, CheckpointStatusChecksumSkipped)
 		} else {
 			err := t.compareChecksum(ctx, rc.tidbMgr.db, cp)
-			rc.saveStatusCheckpoint(t.tableName, nil, CheckpointStatusChecksummed)
+			rc.saveStatusCheckpoint(t.tableName, err, CheckpointStatusChecksummed)
 			if err != nil {
 				common.AppLogger.Errorf("[%s] checksum failed: %v", t.tableName, err.Error())
 				return errors.Trace(err)
@@ -603,6 +671,14 @@ func (rc *RestoreController) fullCompact(ctx context.Context) error {
 		return nil
 	}
 
+	// wait until any existing level-1 compact to complete first.
+	common.AppLogger.Info("Wait for existing level 1 compaction to finish")
+	start := time.Now()
+	for !atomic.CompareAndSwapInt32(&rc.compactState, compactStateIdle, compactStateDoing) {
+		time.Sleep(100 * time.Millisecond)
+	}
+	common.AppLogger.Infof("Wait for existing level 1 compaction to finish takes %v", time.Since(start))
+
 	return errors.Trace(rc.doCompact(ctx, FullLevelCompact))
 }
 
@@ -610,16 +686,19 @@ func (rc *RestoreController) doCompact(ctx context.Context, level int32) error {
 	return errors.Trace(rc.importer.Compact(ctx, level))
 }
 
-func (rc *RestoreController) switchToImportMode(ctx context.Context) error {
-	return errors.Trace(rc.switchTiKVMode(ctx, sstpb.SwitchMode_Import))
+func (rc *RestoreController) switchToImportMode(ctx context.Context) {
+	rc.switchTiKVMode(ctx, sstpb.SwitchMode_Import)
 }
 
 func (rc *RestoreController) switchToNormalMode(ctx context.Context) error {
-	return errors.Trace(rc.switchTiKVMode(ctx, sstpb.SwitchMode_Normal))
+	rc.switchTiKVMode(ctx, sstpb.SwitchMode_Normal)
+	return nil
 }
 
-func (rc *RestoreController) switchTiKVMode(ctx context.Context, mode sstpb.SwitchMode) error {
-	return errors.Trace(rc.importer.SwitchMode(ctx, mode))
+func (rc *RestoreController) switchTiKVMode(ctx context.Context, mode sstpb.SwitchMode) {
+	if err := rc.importer.SwitchMode(ctx, mode); err != nil {
+		common.AppLogger.Warnf("cannot switch to %s mode: %v", mode.String(), err)
+	}
 }
 
 func (rc *RestoreController) checkRequirements(_ context.Context) error {

tidb-lightning.toml
@@ -91,3 +91,11 @@ checksum = true
 compact = true
 # if set true, analyze will do ANALYZE TABLE <table> for each table.
 analyze = true
+
+# cron performs some periodic actions in background
+[cron]
+# duration between which Lightning will automatically refresh the import mode status.
+# should be shorter than the corresponding TiKV setting
+switch-mode = "5m"
+# the duration which the an import progress will be printed to the log.
+log-progress = "5m"

lightning/metric/metric_test.go
@@ -1,0 +1,35 @@
+package metric_test
+
+import (
+	"testing"
+
+	. "github.com/pingcap/check"
+
+	"github.com/pingcap/tidb-lightning/lightning/metric"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type testMetricSuite struct{}
+
+func (s *testMetricSuite) SetUpSuite(c *C)    {}
+func (s *testMetricSuite) TearDownSuite(c *C) {}
+
+var _ = Suite(&testMetricSuite{})
+
+func TestMetric(t *testing.T) {
+	TestingT(t)
+}
+
+func (s *testMetricSuite) TestReadCounter(c *C) {
+	counter := prometheus.NewCounter(prometheus.CounterOpts{})
+	counter.Add(1256.0)
+	counter.Add(2214.0)
+	c.Assert(metric.ReadCounter(counter), Equals, 3470.0)
+}
+
+func (s *testMetricSuite) TestReadHistogramSum(c *C) {
+	histogram := prometheus.NewHistogram(prometheus.HistogramOpts{})
+	histogram.Observe(11131.5)
+	histogram.Observe(15261.0)
+	c.Assert(metric.ReadHistogramSum(histogram), Equals, 26392.5)
+}

tests/checkpoint/run.sh
@@ -75,5 +75,5 @@ echo "******** Verify checkpoint no-op ********"
 PIDFILE="$PIDFILE" run_lightning
 run_sql "$PARTIAL_IMPORT_QUERY"
 check_contains "s: $(( (1000 * $CHUNK_COUNT + 1001) * $CHUNK_COUNT * $TABLE_COUNT ))"
-run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cppk.table_v1 WHERE status = 210"
+run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cppk.table_v1 WHERE status >= 200"
 check_contains "count(*): $TABLE_COUNT"

tests/checkpoint_chunks/run.sh
@@ -52,7 +52,7 @@ PIDFILE="$PIDFILE" run_lightning
 run_sql 'SELECT count(i), sum(i) FROM cpch_tsr.tbl;'
 check_contains "count(i): $(($ROW_COUNT*$CHUNK_COUNT))"
 check_contains "sum(i): $(( $ROW_COUNT*$CHUNK_COUNT*(($CHUNK_COUNT+2)*$ROW_COUNT + 1)/2 ))"
-run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cpch.table_v1 WHERE status = 210"
+run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cpch.table_v1 WHERE status >= 200"
 check_contains "count(*): 1"
 run_sql "SELECT count(*) FROM tidb_lightning_checkpoint_test_cpch.chunk_v3 WHERE pos = end_offset"
 check_contains "count(*): $CHUNK_COUNT"