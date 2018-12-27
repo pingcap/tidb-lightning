57374a03c3540e49be85bab930c5b09c3b708bc6
kennytm <kennytm@gmail.com>
2018-12-28 02:50:40 +0800

    tests: added integration test for CSV

---
+++ tests/csv/run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -eu
+
+run_sql 'DROP DATABASE IF EXISTS csv'
+
+run_lightning
+
+run_sql 'SELECT count(*), sum(PROCESSLIST_TIME), sum(THREAD_OS_ID), count(PROCESSLIST_STATE) FROM csv.threads'
+check_contains 'count(*): 43'
+check_contains 'sum(PROCESSLIST_TIME): 322253'
+check_contains 'sum(THREAD_OS_ID): 303775702'
+check_contains 'count(PROCESSLIST_STATE): 3'
+
+run_sql 'SELECT hex(t), j, hex(b) FROM csv.escapes WHERE i = 1'
+check_contains 'hex(t): 5C'
+check_contains 'j: {"?": []}'
+check_contains 'hex(b): FFFFFFFF'
+
+run_sql 'SELECT hex(t), j, hex(b) FROM csv.escapes WHERE i = 2'
+check_contains 'hex(t): 22'
+check_contains 'j: "\n\n\n"'
+check_contains 'hex(b): 0D0A0D0A'
+
+run_sql 'SELECT hex(t), j, hex(b) FROM csv.escapes WHERE i = 3'
+check_contains 'hex(t): 0A'
+check_contains 'j: [",,,"]'
+check_contains 'hex(b): 5C2C5C2C'
