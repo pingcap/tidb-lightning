7663ea41c19ca8244b16d76d419844594901a5f6
Lonng <chris@lonng.org>
2019-01-21 09:38:47 +0800

    *: add license header

--- tests/exotic_filenames/run.sh
+++ tests/exotic_filenames/run.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+#
+# Copyright 2019 PingCAP, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Confirm the behavior for some exotic filenames
 # Do not enable until https://github.com/pingcap/tidb/pull/8302 is merged.
