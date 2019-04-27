272d967bf1279ddc9fa2e938b00c0eb5d6fa9a9e
kennytm <kennytm@gmail.com>
2019-04-27 21:58:36 +0800

    config: reduce default table-concurrency from 8 to 6
    Ensures table + index <= the default max-open-engine which is 8.

--- lightning/config/config.go
+++ lightning/config/config.go
@@ -156,7 +156,7 @@ func NewConfig() *Config {
 	return &Config{
 		App: Lightning{
 			RegionConcurrency: runtime.NumCPU(),
-			TableConcurrency:  8,
+			TableConcurrency:  6,
 			IndexConcurrency:  2,
 			IOConcurrency:     5,
 			CheckRequirements: true,
