b1848c06c36b3d83111a6b168358d5c755f0c61b
Lonng <chris@lonng.org>
2019-05-14 11:54:36 +0800

    add test for table encoder
    Signed-off-by: Lonng <chris@lonng.org>

--- lightning/kv/sql2kv_test.go
+++ lightning/kv/sql2kv_test.go
@@ -14,8 +14,18 @@
 package kv
 
 import (
+	"errors"
+
 	. "github.com/pingcap/check"
+	"github.com/pingcap/parser/model"
+	"github.com/pingcap/parser/mysql"
+	"github.com/pingcap/tidb-lightning/lightning/log"
+	"github.com/pingcap/tidb/sessionctx"
+	"github.com/pingcap/tidb/table"
+	"github.com/pingcap/tidb/table/tables"
 	"github.com/pingcap/tidb/types"
+	kvenc "github.com/pingcap/tidb/util/kvencoder"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
@@ -41,4 +51,65 @@ func (s *kvSuite) TestMarshal(c *C) {
 	c.Assert(encoder.Fields["bad-test"], DeepEquals, []interface{}{
 		map[string]interface{}{"col": 0, "kind": "min", "val": "-inf"},
 	})
+}
+
+type mockTable struct {
+	table.Table
+}
+
+func (mockTable) AddRecord(ctx sessionctx.Context, r []types.Datum, opts ...*table.AddRecordOpt) (recordID int64, err error) {
+	return -1, errors.New("mock error")
+}
+
+func (s *kvSuite) TestEncode(c *C) {
+	c1 := &model.ColumnInfo{ID: 1, Name: model.NewCIStr("c1"), State: model.StatePublic, Offset: 0, FieldType: *types.NewFieldType(mysql.TypeTiny)}
+	cols := []*model.ColumnInfo{c1}
+	tblInfo := &model.TableInfo{ID: 1, Columns: cols, PKIsHandle: false}
+	tbl := tables.MockTableFromMeta(tblInfo)
+
+	logger := log.Logger{Logger: zap.NewNop()}
+	rows := []types.Datum{
+		types.NewIntDatum(10000000),
+	}
+
+	// Strict mode
+	strictMode := NewTableKVEncoder(tbl, mysql.ModeStrictAllTables)
+	pairs, err := strictMode.Encode(logger, rows, 1, []int{0, 1})
+	c.Assert(err, NotNil)
+	c.Assert(err, ErrorMatches, ".*overflows tinyint")
+	c.Assert(pairs, IsNil)
+
+	rowsWithPk := []types.Datum{
+		types.NewIntDatum(1),
+		types.NewStringDatum("invalid-pk"),
+	}
+	pairs, err = strictMode.Encode(logger, rowsWithPk, 2, []int{0, 1})
+	c.Assert(err, NotNil)
+	c.Assert(err, ErrorMatches, ".*Data Truncated")
+
+	rowsWithPk2 := []types.Datum{
+		types.NewIntDatum(1),
+		types.NewStringDatum("1"),
+	}
+	pairs, err = strictMode.Encode(logger, rowsWithPk2, 2, []int{0, 1})
+	c.Assert(err, IsNil)
+	c.Assert(pairs, DeepEquals, []kvenc.KvPair{{
+		Key: []uint8{0x74, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x5f, 0x72, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+		Val: []uint8{0x8, 0x2, 0x8, 0x2}}})
+
+	// Mock add record error
+	mockTbl := &mockTable{Table: tbl}
+	mockMode := NewTableKVEncoder(mockTbl, mysql.ModeStrictAllTables)
+	pairs, err = mockMode.Encode(logger, rowsWithPk2, 2, []int{0, 1})
+	c.Assert(err, NotNil)
+	c.Assert(err, ErrorMatches, "mock error")
+
+	// Non-strict mode
+	noneMode := NewTableKVEncoder(tbl, mysql.ModeNone)
+	pairs, err = noneMode.Encode(logger, rows, 1, []int{0, 1})
+	c.Assert(err, IsNil)
+	c.Assert(pairs, DeepEquals, []kvenc.KvPair{{
+		Key: []uint8{0x74, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x5f, 0x72, 0x80, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1},
+		Val: []uint8{0x8, 0x2, 0x8, 0xfe, 0x1}}})
+
 }
