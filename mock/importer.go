51e287197c2085f5e0862dd264a1538551ff8bf0
lance6716 <lance6716@gmail.com>
2019-08-26 11:22:55 +0800

    mock: update rpc endpoint

--- mock/importer.go
+++ mock/importer.go
@@ -216,6 +216,26 @@ func (mr *MockImportKVClientMockRecorder) WriteEngine(arg0 interface{}, arg1 ...
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "WriteEngine", reflect.TypeOf((*MockImportKVClient)(nil).WriteEngine), varargs...)
 }
 
+// WriteEngineV3 mocks base method
+func (m *MockImportKVClient) WriteEngineV3(arg0 context.Context, arg1 *import_kvpb.WriteEngineV3Request, arg2 ...grpc.CallOption) (*import_kvpb.WriteEngineResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "WriteEngineV3", varargs...)
+	ret0, _ := ret[0].(*import_kvpb.WriteEngineResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// WriteEngineV3 indicates an expected call of WriteEngineV3
+func (mr *MockImportKVClientMockRecorder) WriteEngineV3(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "WriteEngineV3", reflect.TypeOf((*MockImportKVClient)(nil).WriteEngineV3), varargs...)
+}
+
 // MockImportKV_WriteEngineClient is a mock of ImportKV_WriteEngineClient interface
 type MockImportKV_WriteEngineClient struct {
 	ctrl     *gomock.Controller
