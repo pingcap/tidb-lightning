aaac2b48c3a2954b2c8f6cc1de3d56c1bc41de8e
lance6716 <lance6716@gmail.com>
2019-09-06 16:20:22 +0800

    checkpoint: fix empty map become nil after unmarshall (#237)
    * checkpoint: fix empty map become nil after unmarshall
    
    * checkpoint: unify code style
    
    * checkpoint: remove hard-coded path

--- lightning/checkpoints/checkpoints_test.go
+++ lightning/checkpoints/checkpoints_test.go
@@ -1,6 +1,7 @@
 package checkpoints
 
 import (
+	"path/filepath"
 	"testing"
 
 	. "github.com/pingcap/check"
@@ -287,4 +288,18 @@ func (s *checkpointSuite) TestApplyDiff(c *C) {
 			},
 		},
 	})
+}
+
+func (s *checkpointSuite) TestCheckpointMarshallUnmarshall(c *C) {
+	path := filepath.Join(c.MkDir(), "filecheckpoint")
+	fileChkp := NewFileCheckpointsDB(path)
+	fileChkp.checkpoints.Checkpoints["a"] = &TableCheckpointModel{
+		Status:  uint32(CheckpointStatusLoaded),
+		Engines: map[int32]*EngineCheckpointModel{},
+	}
+	fileChkp.Close()
+
+	fileChkp2 := NewFileCheckpointsDB(path)
+	// if not recover empty map explicitly, it will become nil
+	c.Assert(fileChkp2.checkpoints.Checkpoints["a"].Engines, NotNil)
 }
