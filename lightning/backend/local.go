520e4449e2ead30a2e364ee0bef5c2b406c39080
glorv <glorvs@163.com>
2020-05-22 21:59:09 +0800

    update sort

--- lightning/backend/local.go
+++ lightning/backend/local.go
@@ -602,7 +602,7 @@ WriteAndIngest:
 			regionEnd := region.Region.EndKey
 			_, endKey, _ := codec.DecodeBytes([]byte{}, region.Region.EndKey)
 			endIndex := sort.Search(len(pairs), func(i int) bool {
-				return bytes.Compare(endKey, pairs[i].Key)	< 0
+				return bytes.Compare(pairs[i].Key, endKey)	< 0
 			})
 
 			endKey = codec.EncodeBytes([]byte{}, pairs[endIndex-1].Key)
