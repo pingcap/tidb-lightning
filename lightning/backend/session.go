897a850c3e20830a90025d2621553facdd84539a
luancheng <luancheng@pingcap.com>
2020-06-02 19:21:31 +0800

    address comment & add test

--- lightning/backend/session.go
+++ lightning/backend/session.go
@@ -14,19 +14,13 @@
 package backend
 
 import (
-	"bytes"
 	"context"
 	"strconv"
-	"sync/atomic"
 
-	"github.com/pingcap/errors"
-	"github.com/pingcap/log"
 	"github.com/pingcap/parser/mysql"
 	"github.com/pingcap/tidb/kv"
-	"github.com/pingcap/tidb/kv/memdb"
 	"github.com/pingcap/tidb/sessionctx"
 	"github.com/pingcap/tidb/sessionctx/variable"
-	"go.uber.org/zap"
 
 	"github.com/pingcap/tidb-lightning/lightning/common"
 )
@@ -45,184 +39,24 @@ func (*invalidIterator) Valid() bool {
 func (*invalidIterator) Close() {
 }
 
-type BufferStore struct {
-	MemBuffer *memDbBuffer
-}
-
-// memDbBuffer implements the MemBuffer interface.
-type memDbBuffer struct {
-	sandbox         *memdb.Sandbox
-	entrySizeLimit  int
-	bufferLenLimit  uint64
-	bufferSizeLimit uint64
-}
-
-type memDbIter struct {
-	iter    memdb.Iterator
-	start   []byte
-	end     []byte
-	reverse bool
-}
-
-// Next implements the Iterator Next.
-func (i *memDbIter) Next() error {
-	if i.reverse {
-		i.iter.Prev()
-	} else {
-		i.iter.Next()
-	}
-	return nil
-}
-
-// Valid implements the Iterator Valid.
-func (i *memDbIter) Valid() bool {
-	if !i.reverse {
-		return i.iter.Valid() && (i.end == nil || bytes.Compare(i.Key(), i.end) < 0)
-	}
-	return i.iter.Valid()
-}
-
-// Key implements the Iterator Key.
-func (i *memDbIter) Key() kv.Key {
-	return i.iter.Key()
-}
-
-// Value implements the Iterator Value.
-func (i *memDbIter) Value() []byte {
-	return i.iter.Value()
-}
-
-// Close Implements the Iterator Close.
-func (i *memDbIter) Close() {
-
-}
-
-// NewMemDbBuffer creates a new memDbBuffer.
-func NewMemDbBuffer() *memDbBuffer {
-	return &memDbBuffer{
-		sandbox:         memdb.NewSandbox(),
-		entrySizeLimit:  kv.TxnEntrySizeLimit,
-		bufferSizeLimit: atomic.LoadUint64(&kv.TxnTotalSizeLimit),
-	}
-}
-
-// Iter creates an Iterator.
-func (m *memDbBuffer) Iter(k kv.Key, upperBound kv.Key) (kv.Iterator, error) {
-	i := &memDbIter{
-		iter:    m.sandbox.NewIterator(),
-		start:   k,
-		end:     upperBound,
-		reverse: false,
-	}
-
-	if k == nil {
-		i.iter.SeekToFirst()
-	} else {
-		i.iter.Seek(k)
-	}
-	return i, nil
-}
-
-func (m *memDbBuffer) IterReverse(k kv.Key) (kv.Iterator, error) {
-	return nil, nil
-}
-
-// Get returns the value associated with key.
-func (m *memDbBuffer) Get(ctx context.Context, k kv.Key) ([]byte, error) {
-	return nil, nil
-}
-
-// Set associates key with value.
-func (m *memDbBuffer) Set(k kv.Key, v []byte) error {
-	if len(v) == 0 {
-		return errors.Trace(kv.ErrCannotSetNilValue)
-	}
-	if len(k)+len(v) > m.entrySizeLimit {
-		return kv.ErrEntryTooLarge.GenWithStackByArgs(m.entrySizeLimit, len(k)+len(v))
-	}
-
-	m.sandbox.Put(k, v)
-	if m.Size() > int(m.bufferSizeLimit) {
-		return kv.ErrTxnTooLarge.GenWithStackByArgs(atomic.LoadUint64(&kv.TxnTotalSizeLimit))
-	}
-	return nil
-}
-
-// Delete removes the entry from buffer with provided key.
-func (m *memDbBuffer) Delete(k kv.Key) error {
-	return nil
-}
-
-// Size returns sum of keys and values length.
-func (m *memDbBuffer) Size() int {
-	return m.sandbox.Size()
-}
-
-// Len returns the number of entries in the DB.
-func (m *memDbBuffer) Len() int {
-	return m.sandbox.Len()
-}
-
-func (m *memDbBuffer) NewStagingBuffer() kv.MemBuffer {
-	return &memDbBuffer{
-		sandbox:         m.sandbox.Derive(),
-		entrySizeLimit:  kv.TxnEntrySizeLimit,
-		bufferSizeLimit: m.bufferSizeLimit - uint64(m.sandbox.Size()),
-	}
-}
-
-func (m *memDbBuffer) Flush() (int, error) {
-	// There is no need to check size limit,
-	// because the size limit is maintain when derive this buffer.
-	return m.sandbox.Flush(), nil
-}
-
-// Discard must do nothing because it will discard every time when AddRecord finished
-// It's hard code in TiDB after this PR(https://github.com/pingcap/tidb/pull/15931/files) merged.
-func (m *memDbBuffer) Discard() {
-	// Do nothing
-}
-
-// RealDiscard will truly discard all staging kvs, after lightning translate all kvs into kvpairs
-// It will trigger manually.
-func (m *memDbBuffer) RealDiscard() {
-	m.sandbox.Discard()
-}
-
-// WalkMemBuffer iterates all buffered kv pairs in memBuf
-func WalkMemBuffer(memBuf *memDbBuffer, f func(k kv.Key, v []byte) error) error {
-	iter, err := memBuf.Iter(nil, nil)
-	if err != nil {
-		return errors.Trace(err)
-	}
-
-	defer iter.Close()
-	for iter.Valid() {
-		if err = f(iter.Key(), iter.Value()); err != nil {
-			return errors.Trace(err)
-		}
-		err = iter.Next()
-		if err != nil {
-			return errors.Trace(err)
-		}
-	}
-
-	return nil
-}
-
 // transaction is a trimmed down Transaction type which only supports adding a
 // new KV pair.
 type transaction struct {
 	kv.Transaction
 	kvPairs     []common.KvPair
-	bufferStore *memDbBuffer
 }
 
 func (t *transaction) NewStagingBuffer() kv.MemBuffer {
-	if t.bufferStore == nil {
-		t.bufferStore = NewMemDbBuffer()
-	}
-	return t.bufferStore
+	return t
+}
+
+func (t *transaction) Discard() {
+	// do nothing
+}
+
+func (t *transaction) Flush() (int, error) {
+	// do nothing
+	return 0, nil
 }
 
 // Reset implements the kv.MemBuffer interface
@@ -240,6 +74,10 @@ func (t *transaction) Iter(k kv.Key, upperBound kv.Key) (kv.Iterator, error) {
 
 // Set implements the kv.Mutator interface
 func (t *transaction) Set(k kv.Key, v []byte) error {
+	t.kvPairs = append(t.kvPairs, common.KvPair{
+		Key: k.Clone(),
+		Val: append([]byte{}, v...),
+	})
 	return nil
 }
 
@@ -293,19 +131,8 @@ func newSession(options *SessionOptions) *session {
 }
 
 func (se *session) takeKvPairs() []common.KvPair {
-	bs := se.txn.bufferStore
-	pairs := make([]common.KvPair, 0, bs.Size())
-	err := WalkMemBuffer(bs, func(k kv.Key, v []byte) error {
-		pairs = append(pairs, common.KvPair{
-			Key: k.Clone(),
-			Val: append([]byte{}, v...),
-		})
-		return nil
-	})
-	if err != nil {
-		log.Fatal("failed to take kv pairs from buffer store:", zap.Error(err))
-	}
-	bs.RealDiscard()
+	pairs := se.txn.kvPairs
+	se.txn.kvPairs = make([]common.KvPair, 0, len(pairs))
 	return pairs
 }
 
