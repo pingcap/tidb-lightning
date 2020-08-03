e35948a436b8af3b52417643667c49812cf0e91e
glorv <glorvs@163.com>
2020-08-03 11:19:21 +0800

    fix

--- tests/checkpoint_columns/run.sh
+++ tests/checkpoint_columns/run.sh
@@ -25,7 +25,7 @@ echo 'CREATE DATABASE cp_tsr;' > "$DBPATH/cp_tsr-schema-create.sql"
 PARTIAL_IMPORT_QUERY='SELECT 0'
 
 echo "CREATE TABLE tbl(i TINYINT PRIMARY KEY, j INT);" > "$DBPATH/cp_tsr.tbl-schema.sql"
-echo "INSERT INTO tbl VALUES (1),(2);" > "$DBPATH/cp_tsr.tbl.sql"
+echo "INSERT INTO tbl (i) VALUES (1),(2);" > "$DBPATH/cp_tsr.tbl.sql"
 
 # Set the failpoint to kill the lightning instance as soon as one row is write
 export GO_FAILPOINTS="github.com/pingcap/tidb-lightning/lightning/restore/FailAfterWriteRows=return"
