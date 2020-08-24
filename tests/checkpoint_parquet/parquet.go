bdc42fe11350480064c03109183a35b90cc63e99
glorv <glorvs@163.com>
2020-08-24 12:26:07 +0800

    fix test

--- tests/checkpoint_parquet/parquet.go
+++ tests/checkpoint_parquet/parquet.go
@@ -56,7 +56,7 @@ func main() {
 	flag.Parse()
 
 	for i := 0; i < *chunks; i++ {
-		name := fmt.Sprintf("%s.%s.%04d.parquet", schema, table, i)
+		name := fmt.Sprintf("%s.%s.%04d.parquet", *schema, *table, i)
 		err := genParquetFile(*sourceDir, name, *rowNumbers)
 		if err != nil {
 			log.Fatalf("generate test source failed, name: %s, err: %+v", name, err)
