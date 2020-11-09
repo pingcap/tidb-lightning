f4354c91a7d787d90f8f54904985b67a9f27ae4d
glorv <glorvs@163.com>
2020-11-09 21:16:38 +0800

    mydumper: fix parquet data parser (#435)
    * fix parquet
    
    * reorder imports
    
    * fix test
    
    * use empty collation
    
    * fix a error and add more test cases
    
    * add pointer type tests
    
    * resolve comments
    
    Co-authored-by: kennytm <kennytm@gmail.com>

--- lightning/mydump/parquet_parser_test.go
+++ lightning/mydump/parquet_parser_test.go
@@ -7,10 +7,8 @@ import (
 	"strconv"
 
 	"github.com/pingcap/br/pkg/storage"
-
-	"github.com/pingcap/tidb/types"
-
 	. "github.com/pingcap/check"
+	"github.com/pingcap/tidb/types"
 	"github.com/xitongsys/parquet-go-source/local"
 	writer2 "github.com/xitongsys/parquet-go/writer"
 )
@@ -80,4 +78,129 @@ func (s testParquetParserSuite) TestParquetParser(c *C) {
 	}
 
 	c.Assert(reader.ReadRow(), Equals, io.EOF)
+}
+
+func (s testParquetParserSuite) TestParquetVariousTypes(c *C) {
+	type Test struct {
+		Date            int32 `parquet:"name=date, type=DATE"`
+		TimeMillis      int32 `parquet:"name=timemillis, type=TIME_MILLIS"`
+		TimeMicros      int64 `parquet:"name=timemicros, type=TIME_MICROS"`
+		TimestampMillis int64 `parquet:"name=timestampmillis, type=TIMESTAMP_MILLIS"`
+		TimestampMicros int64 `parquet:"name=timestampmicros, type=TIMESTAMP_MICROS"`
+
+		Decimal1 int32  `parquet:"name=decimal1, type=DECIMAL, scale=2, precision=9, basetype=INT32"`
+		Decimal2 int32  `parquet:"name=decimal2, type=DECIMAL, scale=4, precision=4, basetype=INT32"`
+		Decimal3 int64  `parquet:"name=decimal3, type=DECIMAL, scale=2, precision=18, basetype=INT64"`
+		Decimal4 string `parquet:"name=decimal4, type=DECIMAL, scale=2, precision=10, basetype=FIXED_LEN_BYTE_ARRAY, length=12"`
+		Decimal5 string `parquet:"name=decimal5, type=DECIMAL, scale=2, precision=20, basetype=BYTE_ARRAY"`
+		Decimal6 int32  `parquet:"name=decimal6, type=DECIMAL, scale=4, precision=4, basetype=INT32"`
+	}
+
+	dir := c.MkDir()
+	// prepare data
+	name := "test123.parquet"
+	testPath := filepath.Join(dir, name)
+	pf, err := local.NewLocalFileWriter(testPath)
+	c.Assert(err, IsNil)
+	test := &Test{}
+	writer, err := writer2.NewParquetWriter(pf, test, 2)
+	c.Assert(err, IsNil)
+
+	v := &Test{
+		Date:            18564,              //2020-10-29
+		TimeMillis:      62775123,           // 17:26:15.123
+		TimeMicros:      62775123000,        // 17:26:15.123
+		TimestampMillis: 1603963672356,      // 2020-10-29T17:27:52.356
+		TimestampMicros: 1603963672356956,   //2020-10-29T17:27:52.356956
+		Decimal1:        -12345678,          // -123456.78
+		Decimal2:        456,                // 0.0456
+		Decimal3:        123456789012345678, //1234567890123456.78
+		Decimal4:        "-12345678.09",
+		Decimal5:        "-1234567890123456.78",
+		Decimal6:        -1, // -0.0001
+	}
+	c.Assert(writer.Write(v), IsNil)
+	c.Assert(writer.WriteStop(), IsNil)
+	c.Assert(pf.Close(), IsNil)
+
+	store, err := storage.NewLocalStorage(dir)
+	c.Assert(err, IsNil)
+	r, err := store.Open(context.TODO(), name)
+	c.Assert(err, IsNil)
+	reader, err := NewParquetParser(context.TODO(), store, r, name)
+	c.Assert(err, IsNil)
+	defer reader.Close()
+
+	c.Assert(len(reader.columns), Equals, 11)
+
+	c.Assert(reader.ReadRow(), IsNil)
+	c.Assert(reader.lastRow.Row, DeepEquals, []types.Datum{
+		types.NewCollationStringDatum("2020-10-29", "", 0),
+		types.NewCollationStringDatum("17:26:15.123", "", 0),
+		types.NewCollationStringDatum("17:26:15.123", "", 0),
+		types.NewCollationStringDatum("2020-10-29 17:27:52.356", "", 0),
+		types.NewCollationStringDatum("2020-10-29 17:27:52.356", "", 0),
+		types.NewCollationStringDatum("-123456.78", "", 0),
+		types.NewCollationStringDatum("0.0456", "", 0),
+		types.NewCollationStringDatum("1234567890123456.78", "", 0),
+		types.NewCollationStringDatum("-12345678.09", "", 0),
+		types.NewCollationStringDatum("-1234567890123456.78", "", 0),
+		types.NewCollationStringDatum("-0.0001", "", 0),
+	})
+
+	type TestDecimal struct {
+		Decimal1   int32  `parquet:"name=decimal1, type=DECIMAL, scale=3, precision=5, basetype=INT32"`
+		DecimalRef *int32 `parquet:"name=decimal2, type=DECIMAL, scale=3, precision=5, basetype=INT32"`
+	}
+
+	cases := [][]interface{}{
+		{int32(0), "0.000"},
+		{int32(1000), "1.000"},
+		{int32(-1000), "-1.000"},
+		{int32(999), "0.999"},
+		{int32(-999), "-0.999"},
+		{int32(1), "0.001"},
+		{int32(-1), "-0.001"},
+	}
+
+	fileName := "test.02.parquet"
+	testPath = filepath.Join(dir, fileName)
+	pf, err = local.NewLocalFileWriter(testPath)
+	td := &TestDecimal{}
+	c.Assert(err, IsNil)
+	writer, err = writer2.NewParquetWriter(pf, td, 2)
+	c.Assert(err, IsNil)
+	for i, testCase := range cases {
+		val := testCase[0].(int32)
+		td.Decimal1 = val
+		if i%2 == 0 {
+			td.DecimalRef = &val
+		} else {
+			td.DecimalRef = nil
+		}
+		c.Assert(writer.Write(td), IsNil)
+	}
+	c.Assert(writer.WriteStop(), IsNil)
+	c.Assert(pf.Close(), IsNil)
+
+	r, err = store.Open(context.TODO(), fileName)
+	c.Assert(err, IsNil)
+	reader, err = NewParquetParser(context.TODO(), store, r, fileName)
+	c.Assert(err, IsNil)
+	defer reader.Close()
+
+	for i, testCase := range cases {
+		c.Assert(reader.ReadRow(), IsNil)
+		vals := []types.Datum{types.NewCollationStringDatum(testCase[1].(string), "", 0)}
+		if i%2 == 0 {
+			vals = append(vals, vals[0])
+		} else {
+			vals = append(vals, types.Datum{})
+		}
+		// because we always reuse the datums in reader.lastRow.Row, so we can't directly
+		// compare will `DeepEqual` here
+		eq, err := types.EqualDatums(nil, reader.lastRow.Row, vals)
+		c.Assert(err, IsNil)
+		c.Assert(eq, IsTrue)
+	}
 }
