16c7217a65bcd61983c120830346c41f7cb97815
glorv <glorvs@163.com>
2020-11-25 12:02:19 +0800

    mydumper: convert parquet columns to lower case (#479)
    * convert parquet columns to lower case
    
    * simplify the for loop
    
    Co-authored-by: lance6716 <lance6716@gmail.com>

--- lightning/mydump/parquet_parser.go
+++ lightning/mydump/parquet_parser.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/pingcap/br/pkg/storage"
@@ -86,10 +87,11 @@ func NewParquetParser(
 
 	columns := make([]string, 0, len(reader.Footer.Schema)-1)
 	columnMetas := make([]*parquet.SchemaElement, 0, len(reader.Footer.Schema)-1)
-	for i, c := range reader.SchemaHandler.SchemaElements {
+	for _, c := range reader.SchemaHandler.SchemaElements {
 		if c.GetNumChildren() == 0 {
-			// the SchemaElement.Name is capitalized, we should use the original name
-			columns = append(columns, reader.SchemaHandler.Infos[i].ExName)
+			// NOTE: the SchemaElement.Name is capitalized, SchemaHandler.Infos.ExName is the raw column name
+			// though in this context, there is no difference between these two fields
+			columns = append(columns, strings.ToLower(c.Name))
 			columnMetas = append(columnMetas, c)
 		}
 	}
