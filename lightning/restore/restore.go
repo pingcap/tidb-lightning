9fea8d0c334a1210cf7d60797e838fefe185dedc
__hidehalo <tianchen_cc@yeah.net>
2020-12-29 13:22:14 +0800

    restore: add log for `restoreSchema`

--- lightning/restore/restore.go
+++ lightning/restore/restore.go
@@ -482,6 +482,7 @@ func (worker *restoreSchemaWorker) throw(err error) {
 
 func (rc *RestoreController) restoreSchema(ctx context.Context) error {
 	if !rc.cfg.Mydumper.NoSchema {
+		logTask := log.L().Begin(zap.InfoLevel, "restore all schema")
 		concurrency := 16
 		childCtx, cancel := context.WithCancel(ctx)
 		worker := restoreSchemaWorker{
@@ -497,6 +498,7 @@ func (rc *RestoreController) restoreSchema(ctx context.Context) error {
 			go worker.doJob()
 		}
 		err := worker.wait()
+		logTask.End(zap.ErrorLevel, err)
 		if err != nil {
 			return err
 		}
